c4f3b16573b820778e8c6441acdfc17caa8a7df1
Conrad Bandt <s86201@htw-dresden.de>
2024-03-31 16:26:51 +0200

    feat: dice spawning with random instead of a grid; dice might float around until thrown

--- Assets/Scripts/GetDiceFaceValue.cs
+++ Assets/Scripts/GetDiceFaceValue.cs
@@ -28,6 +28,10 @@ public class GetDiceFaceValue : MonoBehaviour
             {
                 dice.diceFaceNumber=int.Parse(other.name);
                 Debug.Log(dice.diceFaceNumber);
+                
+                
+                
+                
             }
         }
     }
--- Assets/Scripts/PrepareLevel.cs
+++ Assets/Scripts/PrepareLevel.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PrepareLevel : MonoBehaviour
 {
     private GameOptions _options;
     private GameObject _dicePrefab;
+    public GameObject dicePrefab_d4;
     public GameObject dicePrefab_d6;
     public GameObject dicePrefab_d10;
     public GameObject dicePrefab_d20;
     public GameObject spawnAreaPlane;
     public float spawnHeight = 3.5f;
+    public float minForce = -2.5f;
+    public float maxForce = 2.5f;
     private void Awake()
     {
         _options = DataSaver.loadData<GameOptions>("options");
@@ -21,6 +25,9 @@ public class PrepareLevel : MonoBehaviour
         
         switch (_options.diceType)
         {
+            case 4:
+                _dicePrefab = dicePrefab_d4;
+                break;
             case 6:
                 _dicePrefab = dicePrefab_d6;
                 break;
@@ -47,35 +54,33 @@ public class PrepareLevel : MonoBehaviour
         Debug.Log("yspacing"+xSpacing);
         Debug.Log("zspacing:"+zSpacing);
         
-        // create the grid of points
+        // create points to spawn
         List<Vector3> spawnLocations = new List<Vector3>();
         for (int i = 0; i < _options.diceCount; i++)
         {
-            for (int j = 0; j < _options.diceCount; j++)
-            {
-                // TODO this does not work as intended (its a spaced line and not a grid since i run out of diceCount)
-                float xCoord = pointNegative.x + i * xSpacing;
-                float zCoord = pointNegative.z + j * zSpacing;
-                Vector3 point = new Vector3(xCoord, spawnHeight, zCoord);
-                Debug.Log(point);
-                spawnLocations.Add(point);
-            }
+            float xCoord = Random.Range(pointNegative.x, pointPositive.x);
+            float zCoord = Random.Range(pointNegative.z, pointPositive.z);
+            Vector3 point = new Vector3(xCoord, spawnHeight, zCoord);
+            Debug.Log(point);
+            spawnLocations.Add(point);
+            
         }
         
         List<GameObject> allDices = new List<GameObject>();
         for (int i = 0; i < _options.diceCount; i++)
         {
             // spawn and put dices in list
-            Debug.Log(spawnLocations[i]);
+            Debug.Log("Location: "+spawnLocations[i]);
             allDices.Add(Instantiate(_dicePrefab, spawnLocations[i], Quaternion.identity));
+            
             Rigidbody rb = allDices[i].AddComponent<Rigidbody>();
             rb.useGravity = false;
             
             // make dice thowable
             DiceRandomForce forceScript = allDices[i].AddComponent<DiceRandomForce>();
-            forceScript.maxRandomForce = 1f;
-            forceScript.minRandomForce = -1f;
-
+            forceScript.minRandomForce = minForce;
+            forceScript.maxRandomForce = maxForce;
+            
         }
         
     }
